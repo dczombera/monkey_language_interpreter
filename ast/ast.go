94adc1857df894257e0093e936db901e7a1b8a09
Dennis Czombera <milchschaumschluerfer@gmail.com>
2020-03-28 17:40:55 +0100

    Use parser in REPL

--- ast/ast.go
+++ ast/ast.go
@@ -64,7 +64,7 @@ func (ls *LetStatement) String() string {
 		out.WriteString(ls.Value.String())
 	}
 
-	out.WriteString("'")
+	out.WriteString(";")
 
 	return out.String()
 }
--- repl/repl.go
+++ repl/repl.go
@@ -3,12 +3,21 @@ package repl
 import (
 	"bufio"
 	"dczombera/monkey_language_interpreter/lexer"
-	"dczombera/monkey_language_interpreter/token"
+	"dczombera/monkey_language_interpreter/parser"
 	"fmt"
 	"io"
 )
 
 const PROMPT = ">> "
+const MONKEY_FACE = `
+     .-"-.          
+   _/_-.-_\       
+  / __} {__ \    
+ / //  "  \\ \ 
+/ / \'---'/ \ \
+\ \_/'"""'\_/ /
+ \           /
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -23,9 +32,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for t := l.NextToken(); t.Type != token.EOF; t = l.NextToken() {
-			fmt.Printf("%+v\n", t)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, "Parse errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
